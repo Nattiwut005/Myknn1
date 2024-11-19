91b563f6712f330fbe1673fdcb7ed88ed75709cd
Nattiwut005 <155037665+Nattiwut005@users.noreply.github.com>
2024-11-19 14:47:51 +0700

    .

--- home.py
+++ home.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Website Developing using Python")
-st.header("Website Developing using Python")
+st.title("🗨️🗨️Website Developing using Python🗨️🗨️")
+st.header("💟💟Website Developing using Python💟💟")
 
+st.image('./img/jaynat.jpg')
 st.subheader("Nattiwut Nongnuch")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
