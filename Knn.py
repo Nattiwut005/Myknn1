a890c2937ad93aada85f50e669be72d404c28252
Nattiwut005 <155037665+Nattiwut005@users.noreply.github.com>
2024-11-19 15:43:40 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -64,15 +64,15 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.
 
 if st.button("ทำนายผล"):
     #st.write("ทำนาย")
-dt = pd.read_csv("./data/iris.csv") 
-X = dt.drop('variety', axis=1)
-y = dt.variety   
-Knn_model = KNeighborsClassifier(n_neighbors=3)
-Knn_model.fit(X, y)   
-x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
-st.write(Knn_model.predict(x_input))
+    dt = pd.read_csv("./data/iris.csv") 
+    X = dt.drop('variety', axis=1)
+    y = dt.variety   
+    Knn_model = KNeighborsClassifier(n_neighbors=3)
+    Knn_model.fit(X, y)   
+    x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+    st.write(Knn_model.predict(x_input))
 
-out=Knn_model.predict(x_input)
+    out=Knn_model.predict(x_input)
 
 if out[0] == 'Setosa':
     st.image("./pic/iris1.jpg")
