f0487e154fb9fb21fcc9c1fa78672c1a5e62ab16
Nattiwut005 <155037665+Nattiwut005@users.noreply.github.com>
2024-11-19 15:44:50 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -11,16 +11,16 @@ st.header('kairung')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("Versicolor")
-    st.image("./img/iris1.jpg")
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
 
 with col2:
-    st.header("Verginiga")
-    st.image("./img/iris2.jpg")
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
 
 with col3:
-    st.header("Setosa")
-    st.image("./img/iris3.jpg")
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
 
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
@@ -42,9 +42,9 @@ dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
 
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-#st.write(dt.head(10))
-    st.bar_chart(dx2)
-    st.button("ไม่แสดงข้อมูล")
+   #st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
 
@@ -64,21 +64,21 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.
 
 if st.button("ทำนายผล"):
     #st.write("ทำนาย")
-    dt = pd.read_csv("./data/iris.csv") 
-    X = dt.drop('variety', axis=1)
-    y = dt.variety   
-    Knn_model = KNeighborsClassifier(n_neighbors=3)
-    Knn_model.fit(X, y)   
-    x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
-    st.write(Knn_model.predict(x_input))
+   dt = pd.read_csv("./data/iris.csv") 
+   X = dt.drop('variety', axis=1)
+   y = dt.variety   
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)   
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
 
-    out=Knn_model.predict(x_input)
-
-if out[0] == 'Setosa':
+   if out[0] == 'Setosa':
     st.image("./pic/iris1.jpg")
-elif out[0] == 'Versicolor':       
+   elif out[0] == 'Versicolor':       
     st.image("./pic/iris2.jpg")
-else:
+   else:
     st.image("./pic/iris3.jpg")
-    else:
+else:
     st.write("ไม่ทำนาย")
